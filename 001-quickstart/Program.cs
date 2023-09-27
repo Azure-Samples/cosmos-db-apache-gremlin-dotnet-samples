1e814cc954b514ab98512146345f54c82c153cde
Sidney Andrews <sidandrews@microsoft.com>
2023-09-27 17:21:37 +0000

    Add quickstart

---
+++ 001-quickstart/Program.cs
@@ -0,0 +1,59 @@
+﻿// <imports>
+using Gremlin.Net.Driver;
+// </imports>
+
+// <environment_variables>
+string accountName = Environment.GetEnvironmentVariable("COSMOS_GREMLIN_ENDPOINT")!;
+string accountKey = Environment.GetEnvironmentVariable("COSMOS_GREMLIN_KEY")!;
+// </environment_variables>
+
+// <authenticate_client>
+var server = new GremlinServer(
+    hostname: $"{accountName}.gremlin.cosmos.azure.com",
+    port: 443,
+    username: "/dbs/cosmicworks/colls/products",
+    password: $"{accountKey}",
+    enableSsl: true
+);
+
+using var client = new GremlinClient(
+    gremlinServer: server,
+    messageSerializer: new Gremlin.Net.Structure.IO.GraphSON.GraphSON2MessageSerializer()
+);
+// </authenticate_client>
+
+// <create_vertices>
+await client.SubmitAsync(
+    requestScript: "g.addV('product').property('id', '68719518371').property('name', 'Kiama classic surfboard').property('price', 285.55).property('category', 'surfboards')"
+);
+
+await client.SubmitAsync(
+    requestScript: "g.addV('product').property('id', '68719518409').property('name', 'Bondi Twin Surfboard').property('price', 585.50).property('category', 'surfboards')"
+);
+
+await client.SubmitAsync(
+    requestScript: "g.addV('product').property('id', '68719518403').property('name', 'Montau Turtle Surfboard').property('price', 600.00).property('category', 'surfboards')"
+);
+// </create_vertices>
+
+// <create_edges>
+await client.SubmitAsync(
+    requestScript: "g.V(['surfboards', '68719518403']).addE('replaces').to(g.V(['surfboards', '68719518371']))"
+);
+
+await client.SubmitAsync(
+    requestScript: "g.V(['surfboards', '68719518403']).addE('replaces').to(g.V(['surfboards', '68719518409']))"
+);
+// </create_edges>
+
+// <query_vertices_edges>
+var results = await client.SubmitAsync<Dictionary<string, object>>(
+    requestScript: "g.V().hasLabel('product').has('category', 'surfboards').has('name', 'Montau Turtle Surfboard').outE('replaces').inV()"
+);
+
+Console.WriteLine($"[CREATED PRODUCT]\t68719518403");
+foreach (var result in results ?? Enumerable.Empty<Dictionary<string, object>>())
+{
+    Console.WriteLine($"[REPLACES PRODUCT]\t{result["id"]}");
+}
+// </query_vertices_edges>
